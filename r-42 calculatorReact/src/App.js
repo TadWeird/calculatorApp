5bb9e36dce2bb000e297226f071af0ae00242ea2
Good Do <56465252+genegods@users.noreply.github.com>
2021-10-23 18:13:32 +0800

    Add files via upload

---
+++ r-42 calculatorReact/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import CalculatorApp from './components/CalculatorApp'
+
+
+
+function App() {
+  return (
+    
+    <React.Fragment>  
+      <CalculatorApp/>  
+    </React.Fragment>
+     
+       
+  
+  );
+}
+
+export default App;
